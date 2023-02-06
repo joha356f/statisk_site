10857dfcb3536a97160dbeea2a22504c9f2167cb
joha356f <joha356f@stud.kea.dk>
2023-02-06 12:34:35 +0100

    :)

---
+++ script.js
@@ -0,0 +1,24 @@
+const productDataBase = `https://kea-alt-del.dk/t7/api/products/`;
+fetch(productDataBase)
+  .then((res) => res.json())
+  .then(showProduct);
+
+function showProduct(productData) {
+  const gallery = document.querySelector(".product_grid");
+  const template = document.querySelector("#product_card_template").content;
+  productData.forEach((product) => {
+    const templateClone = template.cloneNode(true);
+
+    templateClone.querySelector(".discount").textContent = product.discount;
+
+    templateClone.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+    templateClone.querySelector("img").alt = product.productdisplayname;
+    templateClone.querySelector(".subtle").textContent = product.brandname + product.articletype;
+    templateClone.querySelector(".name").textContent = product.productdisplayname;
+    templateClone.querySelector(".price").textContent = product.price - product.discount;
+    if (product.soldout > 0) {
+      templateClone.querySelector(".small_product").classList.add("soldout");
+    }
+    gallery.appendChild(templateClone);
+  });
+}
